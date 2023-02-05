11fa23dbf94fe0385c866915297ba57b1f4e2c7a
KDBismael <ismaelkondombo1@gmail.com>
2023-02-05 23:45:17 +0100

    Delete comment in store.php file

--- src/store.php
+++ src/store.php
@@ -5,18 +5,12 @@ if(isset($_POST['data'])) {
     $file = fopen('data.json', 'a+');
     $existingData = json_decode(file_get_contents('data.json'));
     if(!empty($existingData)) {
-        // print_r($existingData);
-        // echo '---------------------------------------------------- serparate-----------';
-        // print_r($data);
         $existingData[]=$data;
         $mergedData = $existingData;
     } 
     else {
         $mergedData = array($data);
     }
-    // echo '----------------------------------------------------merged data 1---------------';
-    // print_r($mergedData);
-    // echo '----------------------------------------------------merged data 2---------------';
     ftruncate($file, 0);
     fwrite($file, json_encode($mergedData));
     fclose($file);
