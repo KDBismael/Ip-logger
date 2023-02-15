db0fc28d12c78b596309fbc19b40a3176f11a9d1
KDBismael <ismaelkondombo1@gmail.com>
2023-02-15 11:35:07 +0100

    Update the store.php code

--- src/store.php
+++ src/store.php
@@ -1,18 +1,21 @@
 <?php
-$json_data = file_get_contents('php://input');
+  $json_data = file_get_contents('php://input');
   $data = json_decode($json_data, true);
   $csvFile = 'data.csv';
+  $delimiter = "|";
   if(!empty($data)){
-    if(file_exists($csvFile) && filesize("data.csv") != 0) {
+    if(file_exists($csvFile) && filesize($csvFile) != 0) {
         $fp = fopen($csvFile, 'a');
     } else {
         $fp = fopen($csvFile, 'w');
-        fputcsv($fp, array_keys($data));
+        fputcsv($fp, array_keys($data),$delimiter);
     }
-    fputcsv($fp, $data);
+    $newData=implode("|",$data);
+    fputcsv($fp, array($newData),$delimiter);
+    // fputcsv($fp, $data,$delimiter);
     fclose($fp);
   }
   // Return a response to the JavaScript
-//echo json_encode(array("status" => "success"))
+  // echo json_encode(array("status" => "success"))
 ?>
 
